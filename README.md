a11e851b7364d002518c503334e163fd5f7e40f4
Samuel Wilson <samuel.wilson@fluxfederation.com>
2020-06-15 20:47:51 +1200

    Initialize gap buffer from a given string
    The buffers internal vector was changed from storing characters
    (UTF-32) to unsigned 8-bit integers (u8). Rust treats strings as
    collections of UTF-8 bytes and allows easy conversion of strings
    to vectors of 8-bit integers and vice versa. This seems to be more
    in line with Rust convention.
    
    At the moment it is only possible to add valid UTF-8 to the buffer
    via a string and output valid UTF-8. Future feature additions will
    allow for the insertion of single characters. Currently the buffer
    "unwraps" the conversion of the internal vector to a string. Any
    invalid UTF-8 bytes will cause a panic.

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 # Gap Buffer
 
+## Limitations
+
+The gap buffer stores data as unsigned 8-bit integers suitable for UTF-8 encoding.
+The gap buffer does not check that inserted bytes are valid UTF-8 encodings.
+
 ## License
 
 This repository is open source software under the [MIT license](https://mit-license.org/).
--- src/lib.rs
+++ src/lib.rs
@@ -3,13 +3,19 @@ use std::fmt;
 const DEFAULT_BUFFER_CAPACITY: usize = 10;
 
 struct GapBuffer {
-    buffer: Vec<char>,
+    buffer: Vec<u8>,
 }
 
 impl GapBuffer {
     fn new() -> GapBuffer {
         GapBuffer {
             buffer: Vec::with_capacity(DEFAULT_BUFFER_CAPACITY),
+        }
+    }
+
+    fn from(content: String) -> GapBuffer {
+        GapBuffer {
+            buffer: content.into_bytes(),
         }
     }
 
@@ -24,7 +30,7 @@ impl GapBuffer {
 
 impl fmt::Display for GapBuffer {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "")
+        write!(f, "{}", std::str::from_utf8(&self.buffer).unwrap())
     }
 }
 
@@ -33,6 +39,10 @@ mod tests {
     use GapBuffer;
     use DEFAULT_BUFFER_CAPACITY;
 
+    const TEST_STRING: &str = r"The quick brown
+    fox jumped over
+    the lazy dog.";
+
     #[test]
     fn initialized_empty() {
         let buffer = GapBuffer::new();
@@ -40,5 +50,13 @@ mod tests {
         assert_eq!(buffer.capacity(), DEFAULT_BUFFER_CAPACITY);
         assert_eq!(buffer.len(), 0);
         assert_eq!(buffer.to_string(), "")
+    }
+
+    #[test]
+    fn initialized_from_string() {
+        let buffer = GapBuffer::from(TEST_STRING.to_string());
+
+        assert_eq!(buffer.len(), TEST_STRING.len());
+        assert_eq!(buffer.to_string(), TEST_STRING);
     }
 }
